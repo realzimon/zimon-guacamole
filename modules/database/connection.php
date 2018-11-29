c66663b28d0f658adfdb5cc3a7deef2553021565
Hojosa <mccloud.fox21@gmail.com>
2018-11-29 12:54:39 +0100

    Remove unneeded php scripts

--- modules/database/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $connection = new MySQLi("localhost",  "root", "", "zimon");
-  if ($connection->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $connection->connect_errno . ") " . $connection->connect_error;
-  }
-  ?>
