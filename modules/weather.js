88ec9f5286549ea6ea09626ad70a94b1ecf323cf
Max Bineder <m.bineder@gmail.com>
2018-12-06 11:05:16 +0100

    Weather bugfix

--- modules/weather.js
+++ modules/weather.js
@@ -3,13 +3,14 @@ function showWeather() {
         let weatherId = WeatherData.weather[0].icon + ".png";
         let weatherDescription = WeatherData.weather[0].description;
         let weatherImageUrl = Config.weatherImageUrl + weatherId;
-        let weatherTemp =Math.round((WeatherData.main.temp-273.15)*100)/100;
-        $('#weather').html(weatherTemp+"°C <img src='" + weatherImageUrl + "'> "+weatherDescription);
-    })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            alert('getJSON request failed! ' + textStatus);
-        });
+        let weatherTemp = Math.round((WeatherData.main.temp - 273.15) * 100) / 100;
+        $('#weather').html(weatherTemp + "°C <img src='" + weatherImageUrl + "'> " + weatherDescription);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert('getJSON request failed! ' + textStatus);
+    });
 
+    setInterval(showWeather, 1000*60*5);
 }
+
 
 module.exports.showWeather = showWeather;
