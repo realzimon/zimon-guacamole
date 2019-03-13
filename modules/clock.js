a38b66b1aa547ceb6baeb2a38d8a8cd352f58e6a
nussi98 <nussbaumer.domi@gmail.com>
2019-03-13 12:40:52 +0100

    timer dev and removed localfile

--- modules/clock.js
+++ modules/clock.js
@@ -2,33 +2,47 @@ let end;
 
 function timer(minutes, seconds) {
 	end = addTime(new Date(), minutes, seconds);
-	let timer = document.querySelector("#timer");
+	let timer = document.getElementById("timer");
 	if (Config.timer.text.show) {
 		timer.parentElement.innerHTML = Config.timer.text.value + " <span id='timer'> <!-- TIMER --></span>";
 	}
 	let t = setInterval(function () {
-		let time = new Date((end.getTime()) - (new Date().getTime()));
-		let m = leadingZero(time.getMinutes());
-		let s = leadingZero(time.getSeconds());
-		//timer = document.querySelector("#timer");
-		$("#timer").html(m + ":" + s);
-		//timer.innerHTML = m + ":" + s;
+		time = new Date((end.getTime()) - (new Date().getTime()));
+		console.log({time});
+		m = leadingZero(time.getMinutes());
+		s = leadingZero(time.getSeconds());
 		//Reset
 		if (time.getMinutes() === 0 && time.getSeconds() === 0) {
 			end = addTime(new Date(), minutes, seconds);
-			console.log(new Date());
-			console.log(end);
 			Zivis.shuffleZivis();
 		}
+		timer = document.getElementById("timer");
+		timer.innerHTML = m + ":" + s;
 		if (Vars.war || Vars.edit) {
 			clearInterval(t);
 		}
 	}, 1000);
-	setInterval(() => {
-		end = addTime(new Date(), 10, 0);
-		console.log("timer reload aufgerufen im einstündigen Interval" + new Date());
-	}, 1000 * 60 * 60);
 }
+
+// function timer(minutes, seconds){
+// 	let delay = miliseconds(minutes, seconds);
+// 	console.log(delay);
+
+// 	let expected = Date.now() + delay;
+// 	//setTimeout(step, delay);
+// 	step();
+// 	function step(){
+// 		console.log("step");
+// 		let dt = Date.now() - expected;
+// 		let da = new Date(expected);
+// 		if(dt > delay) {
+// 			console.log("dt > interval", dt, delay);
+// 		}
+// 		expected += delay;
+// 		console.log(da.getMinutes(), da.getSeconds());
+// 		setTimeout(step, Math.max(0, delay - dt));
+// 	}
+// }
 
 function reloadTimer(minutes, seconds) {
 	//Set end to +min, seconds
@@ -37,11 +51,14 @@ function reloadTimer(minutes, seconds) {
 	Zivis.shuffleZivis();
 }
 
+// Umrechnung in Millisekunden
+const miliseconds = (m,s) => (m * 60 + s) * 1000;
+
 function addTime(time, minutesToAdd, secondsToAdd) {
-    time.setMinutes(time.getMinutes() + minutesToAdd);
-    time.setSeconds(time.getSeconds() + secondsToAdd);
-    console.log("addTime aufgerufen ");
-    return time;
+	time.setMinutes(time.getMinutes() + minutesToAdd);
+	time.setSeconds(time.getSeconds() + secondsToAdd);
+	console.log("addTime aufgerufen ", time);
+	return time;
 }
 
 function clock() {
